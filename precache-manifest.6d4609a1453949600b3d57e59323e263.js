a9c14f28068811ad9daf723704ae9ab87bcd09f1
MarioJr92 <mariorodjr92@gmail.com>
2022-10-13 07:11:02 -0400

    Updates

---
+++ precache-manifest.6d4609a1453949600b3d57e59323e263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d51fa42b83f0b37f5db",
+    "url": "/movie-finder-react-router/static/css/main.6b2d9002.chunk.css"
+  },
+  {
+    "revision": "3d51fa42b83f0b37f5db",
+    "url": "/movie-finder-react-router/static/js/main.3d51fa42.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/movie-finder-react-router/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "767e083261ea75cd61c3",
+    "url": "/movie-finder-react-router/static/css/2.1210bc23.chunk.css"
+  },
+  {
+    "revision": "767e083261ea75cd61c3",
+    "url": "/movie-finder-react-router/static/js/2.767e0832.chunk.js"
+  },
+  {
+    "revision": "99864282132ae6a7be47",
+    "url": "/movie-finder-react-router/static/js/runtime~main.99864282.js"
+  },
+  {
+    "revision": "478b5cfc4526a2de0107426c4a70922e",
+    "url": "/movie-finder-react-router/index.html"
+  }
+];
